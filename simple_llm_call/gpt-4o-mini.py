e3eb0fb2141f94cb3d474ded3aa0a3a68a5a9d1a
Nilesh Ramveer <166228951+Fevfunny@users.noreply.github.com>
2026-03-20 14:27:26 +0530

    Update gpt-4o-mini.py

--- simple_llm_call/gpt-4o-mini.py
+++ simple_llm_call/gpt-4o-mini.py
@@ -1,15 +1,21 @@
-# 4.	Use gpt-4o-mini to create 5 SQL interview questions for beginners.
+# 6.	Use gpt-4o-mini to generate Python code for a simple calculator.
 
+from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
 
-llm=ChatOpenAI(model="gpt-4o-mini",
-            base_url="https://openrouter.ai/api/v1",
-            api_key=os.getenv("OPENROUTER_API_KEY"))
+llm = ChatOpenAI(
+    model="gpt-4o-mini",
+    base_url="https://openrouter.ai/api/v1",
+    api_key=os.getenv("OPENROUTER_API_KEY"),
+    temperature=0.2
+)
 
-response=llm.invoke("create 5 SQL interview questions for beginners")
+prompt = PromptTemplate.from_template(
+    template="""Generate complete Python code for a simple calculator"""
+)
 
+response = llm.invoke(prompt.format())
 print(response.content)
